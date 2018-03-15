8eaeec6d9f0c5d33cf6c1fa517127c9e4bf0e3d2
JoshMcCall <joshmccall221@gmail.com>
2018-03-15 12:05:55 -0500

    docs

--- src/ui/components/MyApp/component.ts
+++ src/ui/components/MyApp/component.ts
@@ -1,5 +1,12 @@
 import Component from '@glimmer/component';
 
 export default class MyApp extends Component {
-
+  user = {
+    name: 'Robbie',
+    log: (()=> {
+      debugger;
+      console.log('hello');
+      return 'log';
+    })()
+  };
 }
