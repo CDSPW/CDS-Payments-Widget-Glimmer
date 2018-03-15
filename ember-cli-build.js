6b6cc75954ca325b732f3d988e248d1d9a297b28
JoshMcCall <joshmccall221@gmail.com>
2018-03-15 11:48:20 -0500

    docs

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,9 +2,12 @@
 
 const GlimmerApp = require('@glimmer/application-pipeline').GlimmerApp;
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new GlimmerApp(defaults, {
     // Add options here
+    fingerprint: {
+      prepend: 'https://cdn.rawgit.com/CDSPW/my-app/master/dist/'
+    },
   });
 
   return app.toTree();
